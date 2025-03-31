c6c917f6836a55d6c02882482f4a41c2b3b10a85
fundi1 <wnwlswn23@gmail.com>
2025-03-31 15:39:44 +0900

    get_token

---
+++ 02_get_token.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// 1 인터프리터
+// 02 토큰으로 분류
+char command[100] = {0}; //100단어 내로 명멸어 받기
+char * pointer;
+
+void get_token(){
+    char r = *pointer;
+
+    while(*pointer == '\0' || *pointer == '\n' || *pointer == ' '){//포인터가 빈칸 잡으면 다음칸으로
+        char r = *pointer;
+        pointer ++;   
+    }
+    
+    printf("token = %c\n",r);
+}
+
+int main(){
+    pointer = command;
+    printf(">>>");
+    fgets(command,sizeof(command),stdin); //명령 받기
+    
+    get_token();
+    return 0;
+}
